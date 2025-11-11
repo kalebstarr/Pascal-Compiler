69f767ae958d65e4d485bc11b170908dc1952d37
Kaleb <kaleb.starr@proton.me>
2025-11-11 16:40:06 +0100

    Simplify TokenIterator
    Restrict iterator to []const u8

--- src/root.zig
+++ src/root.zig
@@ -52,7 +52,7 @@ const Special = enum {
     indent,
 };
 
-fn tokenize(comptime T: type, buffer: []const T, delimiters: []const T) TokenIterator(T) {
+fn tokenize(buffer: []const u8, delimiters: []const u8) TokenIterator {
     return .{
         .index = 0,
         .buffer = buffer,
@@ -60,51 +60,58 @@ fn tokenize(comptime T: type, buffer: []const T, delimiters: []const T) TokenIte
     };
 }
 
-fn TokenIterator(comptime T: type) type {
-    return struct {
-        buffer: []const T,
-        delimiter: []const T,
-        index: usize,
+const TokenIterator = struct {
+    buffer: []const u8,
+    delimiter: []const u8,
+    index: usize,
 
-        const Self = @This();
+    const Self = @This();
 
-        pub fn next(self: *Self) ?[]const T {
-            const result = self.peek() orelse return null;
-            self.index += result.len;
-            return result;
+    pub fn next(self: *Self) ?[]const u8 {
+        const result = self.peek() orelse return null;
+        self.index += result.len;
+        return result;
+    }
+
+    pub fn peek(self: *Self) ?[]const u8 {
+        while (self.index < self.buffer.len and self.isDelimiter(self.index)) : (self.index += 1) {}
+        const start = self.index;
+        if (start == self.buffer.len) {
+            return null;
         }
 
-        pub fn peek(self: *Self) ?[]const T {
-            while (self.index < self.buffer.len and self.isDelimiter(self.index)) : (self.index += 1) {}
-            const start = self.index;
-            if (start == self.buffer.len) {
-                return null;
+        var end = start;
+        while (end < self.buffer.len and !self.isDelimiter(end)) : (end += 1) {}
+
+        return self.buffer[start..end];
+    }
+
+    pub fn rest(self: Self) []const u8 {
+        var index: usize = self.index;
+        while (index < self.buffer.len and self.isDelimiter(index)) : (index += 1) {}
+        return self.buffer[index..];
+    }
+
+    pub fn reset(self: *Self) void {
+        self.index = 0;
+    }
+
+    fn isDelimiter(self: Self, index: usize) bool {
+        const item = self.buffer[index];
+        for (self.delimiter) |delimiter_item| {
+            if (item == delimiter_item) {
+                return true;
             }
-
-            var end = start;
-            while (end < self.buffer.len and !self.isDelimiter(end)) : (end += 1) {}
-
-            return self.buffer[start..end];
         }
+        return false;
+    }
+};
 
-        pub fn rest(self: Self) []const T {
-            var index: usize = self.index;
-            while (index < self.buffer.len and self.isDelimiter(index)) : (index += 1) {}
-            return self.buffer[index..];
-        }
+test TokenIterator {
+    var it = TokenIterator{ .buffer = "Jest something", .delimiter = " ", .index = 0 };
+    const first = it.next();
+    const second = it.next();
 
-        pub fn reset(self: *Self) void {
-            self.index = 0;
-        }
-
-        fn isDelimiter(self: Self, index: usize) bool {
-            const item = self.buffer[index];
-            for (self.delimiter) |delimiter_item| {
-                if (item == delimiter_item) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    };
+    try std.testing.expectEqualStrings("Jest", first.?);
+    try std.testing.expectEqualStrings("something", second.?);
 }
